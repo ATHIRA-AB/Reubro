2e4ca3dcb823a0159f0eb1400a71f11f24e8531b
ATHIRA-AB <96428012+ATHIRA-AB@users.noreply.github.com>
2023-09-05 20:40:20 +0530

    Add files via upload

---
+++ adminnew/includes/header.php
@@ -0,0 +1,26 @@
+<div class="header-main">
+					<div class="logo-w3-agile">
+								<h1><a href="dashboard.php">Tourism Management </a></h1>
+							</div>
+				
+						
+						<div class="profile_details w3l">		
+								<ul>
+									<li class="dropdown profile_details_drop">
+									
+											<div class="profile_img">	
+												<span class="prfil-img"><img src="images/User-icon.png" alt=""> </span> 
+												<div class="user-name">
+													<p>Welcome Admin</p>
+													<span>Administrator</span>
+													<break>
+												</div>
+									
+									
+									
+									</li>
+								</ul>
+							</div>
+							
+				     <div class="clearfix"> </div>	
+				</div>
---
+++ adminnew/index.php
@@ -0,0 +1,124 @@
+<?php
+session_start();
+include('includes/config.php');
+if(strlen($_SESSION['alogin'])==0)
+	{	
+header('location:index.php');
+}
+else{
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>TMS | Admin Dashboard</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- Bootstrap Core CSS -->
+<link href="css/bootstrap.min.css" rel='stylesheet' type='text/css' />
+<!-- Custom CSS -->
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link rel="stylesheet" href="css/morris.css" type="text/css"/>
+<!-- Graph CSS -->
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<!-- jQuery -->
+<script src="js/jquery-2.1.4.min.js"></script>
+<!-- //jQuery -->
+<link href='//fonts.googleapis.com/css?family=Roboto:700,500,300,100italic,100,400' rel='stylesheet' type='text/css'/>
+<link href='//fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+<!-- lined-icons -->
+<link rel="stylesheet" href="css/icon-font.min.css" type='text/css' />
+<!-- //lined-icons -->
+</head> 
+<body>
+   <div class="page-container">
+   <!--/content-inner-->
+<div class="left-content">
+	   <div class="mother-grid-inner">
+<!--header start here-->
+<?php include('includes/header.php');?>
+<!--header end here-->
+		<ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="index.php">Home</a> <i class="fa fa-angle-right"></i></li>
+            </ol>
+<!--four-grids here-->
+		<div class="four-grids">
+					
+					<div class="col-md-3 four-grid">
+						<div class="four-w3ls">
+							<div class="icon">
+								<i class="glyphicon glyphicon-folder-open" aria-hidden="true"></i>
+							</div>
+							<div class="four-text">
+								<h3>Enquiries</h3>
+												<?php $sql2 = "SELECT id from tblenquiry";
+$query2= $dbh -> prepare($sql2);
+$query2->execute();
+$results2=$query2->fetchAll(PDO::FETCH_OBJ);
+$cnt2=$query2->rowCount();
+					?>
+								<h4><?php echo htmlentities($cnt2);?></h4>
+								
+							</div>
+							
+						</div>
+					</div>
+					<div class="col-md-3 four-grid">
+						<div class="four-wthree">
+							<div class="icon">
+								<i class="glyphicon glyphicon-briefcase" aria-hidden="true"></i>
+							</div>
+							<div class="four-text">
+								<h3>Toatal packages</h3>
+																	<?php $sql3 = "SELECT PackageId from tbltourpackages";
+$query3= $dbh -> prepare($sql3);
+$query3->execute();
+$results3=$query3->fetchAll(PDO::FETCH_OBJ);
+$cnt3=$query3->rowCount();
+					?>
+								<h4><?php echo htmlentities($cnt3);?></h4>
+								
+							</div>
+							
+						</div>
+					</div>
+						<div class="clearfix"></div>
+				</div>
+
+		
+
+					<div class="clearfix"></div>
+				</div>
+<!--//four-grids here-->
+
+
+			<!--/sidebar-menu-->
+				<?php include('includes/sidebarmenu.php');?>
+							  <div class="clearfix"></div>		
+							</div>
+							<script>
+							var toggle = true;
+										
+							$(".sidebar-icon").click(function() {                
+							  if (toggle)
+							  {
+								$(".page-container").addClass("sidebar-collapsed").removeClass("sidebar-collapsed-back");
+								$("#menu span").css({"position":"absolute"});
+							  }
+							  else
+							  {
+								$(".page-container").removeClass("sidebar-collapsed").addClass("sidebar-collapsed-back");
+								setTimeout(function() {
+								  $("#menu span").css({"position":"relative"});
+								}, 400);
+							  }
+											
+											toggle = !toggle;
+										});
+							</script>
+<!--js -->
+<script src="js/jquery.nicescroll.js"></script>
+
+</body>
+</html>
+<?php } ?>
